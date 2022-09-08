0d11ac70173177e9da4a63f6b41f1063ca705c90
mehrabk <mehrab.kor@gmail.com>
2022-09-08 12:42:57 +0430

    get popular videos

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,20 @@ import styles from "../styles/Home.module.css"
 import Banner from "../components/banner/banner"
 import Navbar from "../components/nav/navbar"
 import SectionCards from "../components/card/section-cards"
-import { useEffect, useState } from "react"
-import { getVideos } from "../lib/videos"
+import { getPopularVideos, getVideos } from "../lib/videos"
 
 export async function getServerSideProps() {
-  const allVideos = await getVideos()
-  console.log(allVideos)
-  return { props: { allVideos } }
+  const disneyVideos = await getVideos("disney")
+  const gotVideos = await getVideos("game of throne")
+  const spidermanVideos = await getVideos("spiderman")
+  const popularVideos = await getPopularVideos()
+
+  return { props: { disneyVideos, gotVideos, spidermanVideos, popularVideos } }
 }
 
 export default function Home(porps) {
-  const { allVideos } = porps
-  const [videos, setVideos] = useState(allVideos)
+  const { disneyVideos, gotVideos, spidermanVideos, popularVideos } = porps
 
-  console.log(videos)
-  useEffect(() => {}, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -26,12 +25,17 @@ export default function Home(porps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar username="mehrab.kor@gmail.com" />
-      <Banner title="Game Of Throne" subTitle="amazing move" imgUrl="/static/images/got.jpg" />
+      <main className={styles.main}>
+        <Navbar username="mehrab.kor@gmail.com" />
+        <Banner title="Game Of Throne" subTitle="amazing move" imgUrl="/static/images/got.jpg" />
 
-      <div className={styles.sectionWrapper}>
-        <SectionCards title="Game Of Throne" videos={videos} size="large" />
-      </div>
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Game Of Throne" videos={gotVideos} size="large" />
+          <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+          <SectionCards title="Spiderman" videos={spidermanVideos} size="small" />
+          <SectionCards title="Popular Videos" videos={popularVideos} size="medium" />
+        </div>
+      </main>
     </div>
   )
 }
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,2 @@
-export const FallbackImage = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+export const FallbackImage =
+  "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
