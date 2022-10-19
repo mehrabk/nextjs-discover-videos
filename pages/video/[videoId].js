2fdc39058eaa53e333fb877b7a08ed7fb264d2a0
mehrabk <mehrab.kor@gmail.com>
2022-10-19 12:10:14 +0330

    modal styled

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
 import Modal from "react-modal"
 import styles from "../../styles/Video.module.css"
+import clsx from "classnames"
+
 Modal.setAppElement("#__next")
 
 const Video = () => {
@@ -18,12 +20,33 @@ const Video = () => {
       >
         <iframe
           id="ytplayer"
+          className={styles.videoPlayer}
           type="text/html"
-          width="640"
+          width="100%"
           height="360"
-          src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com&controls=0&rel=0`}
           frameborder="0"
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>publishTime</p>
+              <p className={styles.title}>Title</p>
+              <p className={styles.description}>description</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>cast:</span>
+                <span className={styles.channelTitle}>subtext</span>
+              </p>
+
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>cast:</span>
+                <span className={styles.channelTitle}>View Count</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   )
